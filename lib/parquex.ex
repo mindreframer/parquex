8f7ee8f6cb0299bb98a1459738084249fd75ec53
Roman Heinrich <roman.heinrich@gmail.com>
2026-08-02 21:57:27 +0200

    Init

---
+++ lib/parquex.ex
@@ -0,0 +1,18 @@
+defmodule Parquex do
+  @moduledoc """
+  Documentation for `Parquex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Parquex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/parquex_test.exs
@@ -0,0 +1,8 @@
+defmodule ParquexTest do
+  use ExUnit.Case
+  doctest Parquex
+
+  test "greets the world" do
+    assert Parquex.hello() == :world
+  end
+end
